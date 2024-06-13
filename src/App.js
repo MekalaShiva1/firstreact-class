460efc509788ece0a8a921ffada6c7f21bcd2710
MekalaShiva1 <mekala.shiva469@gmail.com>
2024-06-13 10:34:15 +0530

    react files added

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+// function App() {
+//   return (  
+//     <h1>hello world</h1>
+
+//   );
+// }
+const New1=()=> {
+  return(
+    <div>
+    
+       <h2 align="Center">Hello second parent </h2> 
+        
+      
+    </div>
+  )
+}  
+
+const hell=()=>{
+  return( 
+    <div>
+    <h2 align="Center">wellcome to react </h2>  
+
+   <p align="center">new line</p> 
+     <New1 />
+   </div>
+    
+  )
+}   
+export default hell
+
+// export default new
+
+
+ 
+ 
+// export default new;
+
